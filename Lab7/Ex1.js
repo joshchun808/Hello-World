dc8101ccb16dd935cdf5452861182f2442b0c350
joshchun808 <joshchun808@gmail.com>
2022-02-22 13:39:19 -1000

    .

--- Lab7/Ex1.js
+++ Lab7/Ex1.js
@@ -1,11 +1,9 @@
 require("./products_data.js");
-var num_products = 5;
 
-var count = 1;
 
-while(count <= num_products) {
-    console.log(`${count}.    ${eval('name' + count)}` );
-    count++;
-    if(count > num_products/2) {break};
+
+for(count = 1; eval("typeof name"+count) != 'undefined' ; count++) {
+    
+    console.log(`${count}.    ${eval('name' + count)}` ); 
 }
-console.log("That\'s all we have!")
+console.log("That's all we have!")
---
+++ Lab8/Invoice1/product1.js
@@ -0,0 +1,4 @@
+// Product 1
+item1 = 'iPhone 7';
+quantity1 = 1;
+price1 = 47.95;
