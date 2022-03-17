6c534d72bf5bcb3772e65926e907897fe62d6a4b
joshchun808 <joshchun808@gmail.com>
2022-03-17 12:32:49 -1000

    .

---
+++ Chun_Joshua_Assignment1/products_data.js
@@ -0,0 +1,30 @@
+//Code from WOD SmartPhonePProducts3
+//List of products 
+
+products = [
+    {
+     'name': "Apple",
+     'price': 350.00,
+     'image': "https://media.istockphoto.com/photos/red-apple-with-leaf-isolated-on-white-background-picture-id185262648?b=1&k=20&m=185262648&s=170667a&w=0&h=2ouM2rkF5oBplBmZdqs3hSOdBzA4mcGNCoF2P0KUMTM=",
+     'quantity_available' : 4,
+    },
+    {
+     'name': "Pear",
+     'price': 275.00,
+     'image': "https://dictionary.cambridge.org/images/thumb/pear_noun_002_26910.jpg?version=5.0.225",
+     'quantity_available' : 4,
+    },
+    {
+     'name': "Banana",
+     'price': 250.00,
+     'image': "https://images.immediate.co.uk/production/volatile/sites/30/2017/01/Bananas-218094b-scaled.jpg?quality=45&resize=768,574",
+     'quantity_available' : 4,
+    },
+    {
+     'name': "Pineapple",
+     'price': 250.00,
+     'image': "https://m.media-amazon.com/images/I/71+qAJehpkL._SL1500_.jpg",
+     'quantity_available' : 4,
+    },
+    
+]
