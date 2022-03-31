99e6e7ca0def1a3aff6266f67ba13244eeaed9eb
joshchun808 <joshchun808@gmail.com>
2022-03-31 13:21:38 -1000

    .

---
+++ Lab13/Ex1b.js
@@ -0,0 +1,15 @@
+var filename = "./user_data.json";
+
+const fs = require("fs");
+
+if(fs.existsSync(filename)) {
+    let stats = fs.statSync(filename);
+    console.log(`${filename} has ${stats.size} characters`);
+    var data = fs.readFileSync(filename, 'utf-8'); 
+    var users = JSON.parse(data);
+    if(typeof users["kazman"] != undefined){
+    console.log(users["kazman"].password);
+    }
+} else {
+    console.log(`${filename} does not exist`)
+}
